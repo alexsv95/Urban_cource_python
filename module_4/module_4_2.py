be61a7607f4de8a8ce3357733f9bf1d6dcdf642c
alexsv95 <a.sosnov95@gmail.com>
2024-09-19 22:50:26 +0300

    task module_4_2 done

---
+++ module_4/module_4_2.py
@@ -0,0 +1,9 @@
+# Домашнее задание по уроку "Пространство имен"
+
+
+def test_function():
+    def inner_function():
+        print('Я в области видимости функции test_function')
+    inner_function()
+
+inner_function()
