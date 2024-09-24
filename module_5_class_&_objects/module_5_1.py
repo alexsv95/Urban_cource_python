7c7d9018ff012c3bb3ce85cb7f4b3a3895c6c34b
alexsv95 <a.sosnov95@gmail.com>
2024-09-24 10:26:55 +0300

    Edited method go_to_floor

--- module_5_class_&_objects/module_5_1.py
+++ module_5_class_&_objects/module_5_1.py
@@ -1,13 +1,13 @@
 # Задача "Developer - не только разработчик"
 
 class House:
-    def __init__(self, name, number_of_floor):
+    def __init__(self, name, number_of_floors):
         self.name = name
-        self.number_of_floor = number_of_floor
+        self.number_of_floors = number_of_floors
 
     def go_to_floor(self, new_floor: int):
-        if new_floor > self.number_of_floor:
-            print(f'{new_floor}-го этажа в доме "{self.name}" не существует, самый высокий этаж {self.number_of_floor}')
+        if new_floor > self.number_of_floors or new_floor < 1:
+            print(f'{new_floor}-го этажа в доме "{self.name}" не существует, самый высокий этаж {self.number_of_floors}')
         else:
             for i in range(1, new_floor + 1):
                 print(i)
@@ -16,4 +16,4 @@ class House:
 h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
 h1.go_to_floor(5)
-h2.go_to_floor(10)
+h2.go_to_floor(0)
