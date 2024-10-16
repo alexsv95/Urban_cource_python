4e1cff5db10bb5bea01050295b7ce571da38c874
alexsv95 <a.sosnov95@gmail.com>
2024-10-16 23:04:56 +0300

    task module_7_4 done

---
+++ module_7_files/module_7_4.py
@@ -0,0 +1,33 @@
+team1_name = 'Мастера кода'
+team2_name = 'Волшебники данных'
+team1_num = 5
+team2_num = 6
+score_1 = 40
+score_2 = 42
+team1_time = 1552.512
+team2_time = 2153.31451
+tasks_total = score_1 + score_2
+time_avg = (team1_time + team2_time) / 2
+
+def winner_definition() -> str:
+    if score_1 > score_2 or score_1 == score_2 and team1_time > team2_time:
+        challenge_result = f'Победа команды {team1_name}!'
+    elif score_1 < score_2 or score_1 == score_2 and team1_time < team2_time:
+        challenge_result = f'Победа команды {team2_name}!'
+    else:
+        challenge_result = 'Ничья!'
+    return challenge_result
+
+
+# Использование %
+print('В команде Мастера кода участников: %s!' % team1_num)
+print('Итого сегодня в командах участников: %(team1)s и %(team2)s!' % {'team1': team1_num, 'team2': team2_num})
+
+# Использование format()
+print('Команда Волшебники данных решила задач: {}!'.format(score_2))
+print('Волшебники данных решили задачи за {tt1}с!'.format(tt1=team1_time))
+
+# Использование f-строк
+print(f'Команды решили {score_1} и {score_2} задач.')
+print(f'Результат битвы: {winner_definition()}')
+print(f'Сегодня было решено {tasks_total} задач, в среднем по {time_avg} секунды на задачу!.')
