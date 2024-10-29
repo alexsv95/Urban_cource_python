35dab656e36e6928c1362f3fdd96e45ac119c2df
alexsv95 <a.sosnov95@gmail.com>
2024-10-29 23:08:24 +0300

    task module_9_1 done

---
+++ module_9_func_prog/module_9_1.py
@@ -0,0 +1,11 @@
+
+def apply_all_func(int_list, *function):
+    result = {}
+    for func in function:
+        result[func.__name__] = func(int_list)
+    return result
+
+my_numbers = [7, 2, 9, 4, 1, 20, 6, 8]
+
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
