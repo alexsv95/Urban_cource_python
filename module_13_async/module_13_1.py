8087d52c7099f02c679cf151d25f788778125007
alexsv95 <a.sosnov95@gmail.com>
2024-12-09 11:38:22 +0300

    task module_13_1 done

--- module_13_async/module_13_1.py
+++ module_13_async/module_13_1.py
@@ -5,7 +5,7 @@ async def start_strongman(name, power):
     for i in range(1, 6):
         await asyncio.sleep(1 / power)
         print(f'Силач {name} поднял {i} шар')
-    print(f'Силам {name} закончил соревнования')
+    print(f'Силач {name} закончил соревнования')
 
 async def start_tournament():
     task1 = asyncio.create_task(start_strongman('Pasha', 3))
