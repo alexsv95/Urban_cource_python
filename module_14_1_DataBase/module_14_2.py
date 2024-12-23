1af53dd4468e646d47d89575ecba79a48b132963
alexsv95 <a.sosnov95@gmail.com>
2024-12-23 22:07:02 +0300

    task module_14_2 done

---
+++ module_14_1_DataBase/module_14_2.py
@@ -0,0 +1,47 @@
+import sqlite3
+
+connection = sqlite3.connect("not_telegram.db")
+cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER,
+balance INTEGER NOT NULL
+)
+''')
+
+# module_14_1
+# for i in range(1, 11):
+#     parameters = f"user{i}", f"example{i}@gmail.com", f"{i*10}", "1000"
+#     cursor.execute("INSERT INTO Users(username, email, age, balance) VALUES (?, ?, ?, ?)", parameters)
+#
+# cursor.execute("SELECT id FROM Users")
+# users = cursor.fetchall()
+#
+# for i in users:
+#     if i[0] % 2 != 0:
+#         cursor.execute(f"UPDATE Users SET balance = ? WHERE id = {i[0]}", (500,))\
+#
+# id = 1
+# for i in users:
+#     cursor.execute(f"DELETE FROM Users WHERE id = {id}")
+#     id += 3
+#
+# cursor.execute("SELECT * FROM Users WHERE age != ?", (60,))
+# users_2 = cursor.fetchall()
+# for user in users_2:
+#     print(f'имя: {user[1]} | почта: {user[2]} | возраст {user[3]} | баланс: {user[4]}')
+
+# module_14_2
+cursor.execute("DELETE FROM Users WHERE id=?", ("6",))
+cursor.execute("SELECT COUNT(*) FROM Users")
+total_users = cursor.fetchone()[0]
+cursor.execute("SELECT SUM(balance) FROM Users")
+all_balance = cursor.fetchone()[0]
+print(all_balance / total_users)
+
+connection.commit()
+connection.close()
